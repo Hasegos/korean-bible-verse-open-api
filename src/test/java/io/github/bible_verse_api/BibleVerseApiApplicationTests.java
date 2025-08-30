72817449f3187e9f795dc41777cdac26f746c805
Hasegos <csw020106@naver.com>
2025-08-31 00:19:39 +0900

    ADD Initial settings

---
+++ src/test/java/io/github/bible_verse_api/BibleVerseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bible_verse_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleVerseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
