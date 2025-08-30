6379090f7e338ad6a4990f240ef5336989c896a1
Hasegos <csw020106@naver.com>
2025-08-31 00:22:30 +0900

    ADD Initial settings

---
+++ src/main/java/io/github/bible_verse_api/BibleVerseApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.bible_verse_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleVerseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleVerseApiApplication.class, args);
+	}
+
+}
