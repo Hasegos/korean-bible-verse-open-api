6c9d7f7ea7b1509370ce83d37b1ed1075124cdac
Hasegos <csw020106@naver.com>
2025-08-31 11:10:13 +0900

    FIX Service 구조 전체적인 개선
    @PostConstruct를 통해 작업

--- src/main/java/io/github/bible_verse_api/service/BibleVerseService.java
+++ src/main/java/io/github/bible_verse_api/service/BibleVerseService.java
@@ -1,22 +1,37 @@
 package io.github.bible_verse_api.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.bible_verse_api.model.BibleVerse;
-import io.github.bible_verse_api.repository.BibleVerseRepository;
-import lombok.RequiredArgsConstructor;
+import jakarta.annotation.PostConstruct;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 @Service
-@RequiredArgsConstructor
 public class BibleVerseService {
 
-    private final BibleVerseRepository repo;
-    private final Random random = ThreadLocalRandom.current();
+    private List<BibleVerse> verses = Collections.emptyList();
+    private final Random random = new Random();
+
+    @PostConstruct
+    public void init() throws IOException{
+        ObjectMapper objectMapper = new ObjectMapper();
+        try (InputStream is = new ClassPathResource("verses.json").getInputStream()){
+            this.verses = objectMapper.readValue(is, new TypeReference<>() {});
+        }
+    }
 
     public BibleVerse getRandomVerse(){
-        var list = repo.findAll();
-        return list.get(random.nextInt(list.size()));
+        if(verses.isEmpty()){
+            return new BibleVerse("로딩 실패", "0", "0", "성경 구절 데이터를 불러오는데 실패했습니다.");
+        }
+
+        return verses.get(random.nextInt(verses.size()));
     }
 }
