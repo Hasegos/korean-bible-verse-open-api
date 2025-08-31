faf68fe5c5e9f06d555e25031d4ae05c880cfc18
Hasegos <csw020106@naver.com>
2025-08-31 11:08:20 +0900

    DEL repository 계층적인 구조삭제
    vercel 배포간에 충돌나는 repository 삭제

--- src/main/java/io/github/bible_verse_api/repository/BibleVerseRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.bible_verse_api.repository;
-
-import io.github.bible_verse_api.model.BibleVerse;
-
-import java.util.List;
-
-public interface BibleVerseRepository {
-    List<BibleVerse> findAll();
-}
